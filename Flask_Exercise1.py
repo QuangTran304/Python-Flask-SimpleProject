eef604c2e65b7e776c2a0e27d250baa07423cc32
Quang Tran <nhatquang304@gmail.com>
2019-05-15 23:22:46 -0400

    Exercise-1 completed

---
+++ Flask_Exercise1.py
@@ -0,0 +1,18 @@
+# -------------------------------------------------------------------------
+#   Author: Quang Tran
+#   Date: May 15th, 2019
+# -------------------------------------------------------------------------
+
+from flask import Flask
+
+# Write __name__ inside to let Flask app identify the root path (later will need)
+app = Flask(__name__)
+
+
+@app.route('/')     # Whatever name we place after the slash '/' will be a web page we want to do something with
+def hello():        # By convention, name this func the same name as your web page
+    return "Welcome to my home page"
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
